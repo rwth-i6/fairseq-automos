dbfa2d77a6757d5882c1d0cee52cecd27c5900fc
Mattia Di Gangi <mdigangi@apptek.com>
2022-10-28 08:58:19 -0400

    Save the model in the folder expected by predict.py

--- download_w2v_small.sh
+++ download_w2v_small.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ ! -f "fairseq_model/wav2vec_small.pt" ]; then
     mkdir -p fairseq_model
-    wget https://dl.fbaipublicfiles.com/fairseq/wav2vec/wav2vec_small.pt -P fairseq
-    wget https://raw.githubusercontent.com/pytorch/fairseq/main/LICENSE -P fairseq/
+    wget https://dl.fbaipublicfiles.com/fairseq/wav2vec/wav2vec_small.pt -P fairseq_model/
+    wget https://raw.githubusercontent.com/pytorch/fairseq/main/LICENSE -P fairseq_model/
 fi
 
 if [ ! -f "pretrained/ckpt_w2vsmall" ]; then
